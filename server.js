902a790348065351aee485911fa41301a64fcaf2
carla <cfpcarla@gmail.com>
2020-02-16 18:11:53 -0500

    add login to the server

--- server.js
+++ server.js
@@ -1,55 +1,83 @@
-// load .env data into process.env
 require('dotenv').config();
-
-// Web server config
-const PORT       = process.env.PORT || 8080;
-const ENV        = process.env.ENV || "development";
-const express    = require("express");
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
 const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
-const app        = express();
-const morgan     = require('morgan');
-
-// PG database client/connection setup
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const cookieSession = require('cookie-session');
 const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
+const dbParams = require('./public/lib/db.js');
+console.log(dbParams)
 const db = new Pool(dbParams);
 db.connect();
-
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
+
+// Configure CORS to accepts requests from any client
+// In the future I should use the corsOptions to accept requests only from my client, but it is not working.
+// CORS: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+// const corsOptions = {
+//   origin: 'http://localhost:3002/'
+// }
+app.use(cors())
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
 }));
-app.use(express.static("public"));
 
-// Separated Routes for each Resource
-// Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
-
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
-// Note: mount other resources here, using the same pattern above
-
-
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
-  res.render("index");
+//home
+app.get('/', (request, response) => {
+  response.send("xunda");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+app.post('/login', (request, response) => {
+  console.log(request.body)
+  db.query(`SELECT id, email, password
+            FROM users
+            WHERE email = $1;`, [request.body.email])
+    .then(data => {
+      const user = data.rows[0];
+      if (!user) {
+        response.statusCode = 403;
+        response.end('403 Forbidden. E-mail cannot be found');
+      } else if (!bcrypt.compareSync(request.body.password, user.password)) {
+        response.statusCode = 403;
+        response.end('403 Forbidden. Wrong password');
+      } else {
+        response.statusCode = 200;
+        response.end(`success. user: ${user}`);
+      }
+      response.json({ user });
+    })
+    .catch (err => {
+      response
+        .status(500)
+        .json({ error: err.message });
+  });
+});
+
+//POST LOGOUT
+app.post('/logout', (request, response) => {
+  request.session.user_id = null;
+  response.redirect('/');
+});
+
+// server.post('/login', passport.authenticate('local'), (req, res, next) => {
+//   if (req.user) {
+//       let redir = { redirect: "/" };
+//       return res.json(redir);
+// } else {
+//       let redir = { redirect: '/login'};
+//       return res.json(redir);
+// }
+// })
