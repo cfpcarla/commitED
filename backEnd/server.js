ec3ca1701a29bd64f781473eb0768b9b1817d8d9
carla <cfpcarla@gmail.com>
2020-02-16 20:08:37 -0500

    Create and fineshed register post

--- backEnd/server.js
+++ backEnd/server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session');
 const { Pool } = require('pg');
-const dbParams = require('./backEnd/public/lib/db.js.js');
+const dbParams = require('./lib/db.js');
 console.log(dbParams)
 const db = new Pool(dbParams);
 db.connect();
@@ -40,6 +40,7 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
 
+//POST login
 app.post('/login', (request, response) => {
   console.log(request.body)
   db.query(`SELECT id, email, password
@@ -71,6 +72,47 @@ app.post('/logout', (request, response) => {
   request.session.user_id = null;
   response.redirect('/');
 });
+
+//POST Register
+app.post('/register', (request, response) => {
+  const email = request.body.email;
+  const password = request.body.password;
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  if (email === '' || password === '') {
+    response.statusCode = 400;
+    response.end('400 Bad request. Missing email or password');
+    return;
+  }
+  db.query(`SELECT email
+  FROM users
+  WHERE email = $1;`, [request.body.email])
+    .then(data => {
+      const user = data.rows[0];
+      if (user) {
+        response.statusCode = 400;
+        response.end('400 Bad request. Email already registered');
+      } else {
+        db.query(`INSERT INTO users(name, address, phone, email, password) VALUES($1,$2,$3,$4,$5) RETURNING *;`,
+          [request.body.name,request.body.address, request.body.phone, request.body.email, hashedPassword])
+          .then(data => {
+            const newUser = data.rows[0];
+            // eslint-disable-next-line camelcase
+            request.session.user_id = newUser.id;
+            response.statusCode = 200;
+            response.end(`success. user: ${user}`);
+          });
+      }
+    });
+});
+
+
+
+
+
+
+
 
 // server.post('/login', passport.authenticate('local'), (req, res, next) => {
 //   if (req.user) {
