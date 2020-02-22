600f3c9e522617d5b5f090841533f55f34312e5e
carla <cfpcarla@gmail.com>
2020-02-21 21:18:11 -0500

    Fix opportunities

--- frontend/src/components/PostsForm/PostsForm.js
+++ frontend/src/components/PostsForm/PostsForm.js
@@ -13,6 +13,7 @@ import CardBody from "../Card/CardBody";
 import CardFooter from "../Card/CardFooter";
 import CustomInput from "../CustomInput/CustomInput";
 import axios from "axios";
+import qs from 'qs';
 
 import styles from "../../assets/jss/material-kit-react/views/loginPage";
 
@@ -21,13 +22,17 @@ const useStyles = makeStyles(styles);
 
 export default function CreatePosts(props) {
   const [state, setState] = useState( {
-    type:'',
-    description:"",
-    title:'',
-    date_posted:Date.now(),
-    user_id: 1, //organization_id goes in here
-    address:''
+
+    // type: "",
+    // description:"",
+    // title:'',
+    // date_posted:Date.now(),
+    // user_id: 1, //organization_id goes in here
+    // address:''
   })
+  console.log(state)
+  console.log(useState)
+
   // constructor(props) {
   //   super(props);
   //   this.state = {
@@ -44,12 +49,19 @@ export default function CreatePosts(props) {
   //   this.handleInputChange = this.handleInputChange.bind(this);
   // }
   function newPost (event)  {
-     event.preventDefault()
-    axios.post(`/api/posts/new`, state).then(res => {
+    event.preventDefault()
+    axios.post(`/api/posts/new`, qs.stringify({
+      type: event.target.type.value,
+      date_posted:Date.now(),
+      user_id: 1,
+      address: event.target.address.value,
+      title: event.target.title.value,
+      description: event.target.description.value,
+    })).then(res => {
       setState(res.data);
     });
   }
-
+  console.log("sa merda", state)
   function handleInputChange(event) {
     const target = event.target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
@@ -69,150 +81,130 @@ export default function CreatePosts(props) {
 
   return (
     <Card>
-      <form>
-        <CardBody>
-          <CustomInput
-            labelText="Category"
-            id="type"
-            value={state.type}
-            onChange={handleInputChange}
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              type: "text"
-            }}
-          />
-          <CustomInput
-            labelText="Position Name"
-            id="title"
-            value={state.title}
-            onChange={handleInputChange}
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              type: "text"
+    <form onSubmit={newPost}>
+    <CardBody>
+    <CustomInput
+    labelText="Category"
+    id="type"
+    value={state.type}
+    onChange={handleInputChange}
+    formControlProps={{
+      fullWidth: true
+    }}
+    inputProps={{
+      type: "text"
+    }}
+    />
+    <CustomInput
+    labelText="Position Name"
+    id="title"
+    value={state.title}
+    onChange={handleInputChange}
+    formControlProps={{
+      fullWidth: true
+    }}
+    inputProps={{
+      type: "text"
 
-            }}
-          />
-          <CustomInput
-            labelText="Description"
-            id="description"
-            value={state.description}
-            onChange={handleInputChange}
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              type: "text"
-            }}
-          />
-          <CustomInput
-            labelText="Address"
-            id="address"
-              value={state.address}
-              onChange={handleInputChange}
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              type: "address",
-              autoComplete: "off"
-            }}
-          />
-           <CustomInput
-            labelText="Hours"
-            id="hours"
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              type: "text",
-            }}
-          />
-           <CustomInput
-            labelText="Number of opportunities"
-            id="vacancies"
-            formControlProps={{
-              fullWidth: true
-            }}
-            inputProps={{
-              type: "text",
-              autoComplete: "off"
-            }}
-          />
-        </CardBody>
-        <CardFooter className={classes.cardFooter}>
-          <Button onClick={newPost} type="submit" simple color="primary" size="lg">
-            Submit
-          </Button>
-        </CardFooter>
-      </form>
+    }}
+    />
+    <CustomInput
+    labelText="Description"
+    id="description"
+    value={state.description}
+    onChange={handleInputChange}
+    formControlProps={{
+      fullWidth: true
+    }}
+    inputProps={{
+      type: "text"
+    }}
+    />
+    <CustomInput
+    labelText="Address"
+    id="address"
+    value={state.address}
+    onChange={handleInputChange}
+    formControlProps={{
+      fullWidth: true
+    }}
+    inputProps={{
+      type: "address",
+      autoComplete: "off"
+    }}
+    />
+
+    </CardBody>
+    <CardFooter className={classes.cardFooter}>
+    <Button type="submit" simple color="primary" size="lg">
+    Submit
+    </Button>
+    </CardFooter>
+    </form>
     </Card>
-  );
-}
+    );
+  }
 
-//async not needed - rodrigo
-// const [newPosts, setNewPosts] =  useState([]);
-// const newPost =  () => {
-//   axios.post(`http://localhost:8080/posts/new`, this.state).then(res => {
-//     setNewPosts(res.data);
-//   });
-// };
+  //async not needed - rodrigo
+  // const [newPosts, setNewPosts] =  useState([]);
+  // const newPost =  () => {
+  //   axios.post(`http://localhost:8080/posts/new`, this.state).then(res => {
+  //     setNewPosts(res.data);
+  //   });
+  // };
 
 
-//console.log("new posts in app.jds",newPosts)
+  //console.log("new posts in app.jds",newPosts)
 
-// export default class CreatePosts extends React.Component {
+  // export default class CreatePosts extends React.Component {
 
-//   constructor(props) {
-//     super(props);
-//     this.state = {
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
 
-//       type:'',
-//       description:"",
-//       title:'',
-//       date_posted:Date.now(),
-//       user_id: 1,
-//       address:''
+  //       type:'',
+  //       description:"",
+  //       title:'',
+  //       date_posted:Date.now(),
+  //       user_id: 1,
+  //       address:''
 
-//     };
-//     this.newPost = this.newPost.bind(this)//refers to component not function itself
-//     this.handleInputChange = this.handleInputChange.bind(this);
-//   }
-//    newPost (event)  {
-//      event.preventDefault()
-//     axios.post(`http://localhost:8080/posts/new`, this.state).then(res => {
-//       this.setState(res.data);
-//     });
-//   }
+  //     };
+  //     this.newPost = this.newPost.bind(this)//refers to component not function itself
+  //     this.handleInputChange = this.handleInputChange.bind(this);
+  //   }
+  //    newPost (event)  {
+  //      event.preventDefault()
+  //     axios.post(`http://localhost:8080/posts/new`, this.state).then(res => {
+  //       this.setState(res.data);
+  //     });
+  //   }
 
-//   handleInputChange(event) {
-//     const target = event.target;
-//     const value = target.type === 'checkbox' ? target.checked : target.value;
-//     const name = target.name;
+  //   handleInputChange(event) {
+  //     const target = event.target;
+  //     const value = target.type === 'checkbox' ? target.checked : target.value;
+  //     const name = target.name;
 
-//     this.setState({
-//       [name]: value
-//     });
-//   }
+  //     this.setState({
+  //       [name]: value
+  //     });
+  //   }
 
-//   render() {
-//     console.log(this.state)
+  //   render() {
+  //     console.log(this.state)
 
-//     return (
-//       <form>
+  //     return (
+  //       <form>
 
-//         {/* <br />
-//         <label>
-//           Category:
-//           <input
-//             name="type"
-//             type="text"
-//             value={this.state.type}
-//             onChange={this.handleInputChange} />
-//         </label>
+  //         {/* <br />
+  //         <label>
+  //           Category:
+  //           <input
+  //             name="type"
+  //             type="text"
+  //             value={this.state.type}
+  //             onChange={this.handleInputChange} />
+  //         </label>
 
 //         <br /> */}
 //         {/* <label>
